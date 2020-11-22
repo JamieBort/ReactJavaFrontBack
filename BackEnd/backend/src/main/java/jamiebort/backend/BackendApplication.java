bd8a3da7b065b18d0c8c557d56ddd8868d93c0e2
Jamie Bort <jamiebort@gmail.com>
2020-11-22 11:47:34 -0500

    Successfully added the hello controller. Added the BackEnd/backend/src/main/java/jamiebort/backend/hello/ directory.

--- BackEnd/backend/src/main/java/jamiebort/backend/BackendApplication.java
+++ BackEnd/backend/src/main/java/jamiebort/backend/BackendApplication.java
@@ -11,3 +11,18 @@ public class BackendApplication {
 	}
 
 }
+
+// package io.javabrains.courseapidata;
+// // note, for reference, in the tutorial the classpath is io.javabrains.springbootstarter
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class CourseApiDataApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(CourseApiDataApplication.class, args);
+// 	}
+
+// }
