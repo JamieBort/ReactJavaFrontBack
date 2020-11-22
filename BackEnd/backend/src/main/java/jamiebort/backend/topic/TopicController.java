c0016a3e11d8fba1cda1f37d44be1fad204e759e
Jamie Bort <jamiebort@gmail.com>
2020-11-22 11:51:27 -0500

    Successfully added the BackEnd/backend/src/main/java/jamiebort/backend/topic/ directory.

---
+++ BackEnd/backend/src/main/java/jamiebort/backend/topic/TopicController.java
@@ -0,0 +1,44 @@
+// package io.javabrains.courseapidata.topic;
+package jamiebort.backend.topic;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
+    @RequestMapping("/topics") // The default request is GET.
+    public List<Topic> getAllTopics(){
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}") // The default request is GET.
+    public Topic getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics") // POST request explicitly written.
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method=RequestMethod.PUT, value="/topics/{id}") // PUT request explicitly written.
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+        topicService.updateTopic(id, topic);
+    }
+
+    @RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}") // DELETE request explicitly written.
+    public void deleteTopic(@PathVariable String id){
+        topicService.deleteTopic(id);
+    }
+
+}
---
+++ BackEnd/backend/src/main/java/jamiebort/backend/topic/TopicService.java
@@ -0,0 +1,58 @@
+// package io.javabrains.courseapidata.topic;
+package jamiebort.backend.topic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    // modified in video 29
+    public List<Topic> getAllTopics() { // GET request
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add); // look into method references. free resource mentioned in video
+                                                        // 29 Java 8 Lambda Basics on Java Brains at javabrains.io
+        return topics;
+        // return topics;
+    }
+
+    // modified in video 29
+    public Topic getTopic(String id) {
+        // in the tutorial it was `return topicRepository.findOne(id);`. 
+        // findOne() is no longer available. It is replaced by findById().
+        // findById() requires the return type to be `Optional<Topic>` rather than `Topic`.
+        return topicRepository.findById(id).get();
+        // return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    // modified in video 29
+    public void addTopic(Topic topic) {
+        topicRepository.save(topic);
+        // topics.add(topic);
+    }
+
+    // modified in video 29
+    public void updateTopic(String id, Topic topic) {
+        topicRepository.save(topic);
+        // for(int i=0; i<topics.size(); i++){
+        //     Topic t = topics.get(i);
+        //     if(t.getId().equals(id)){
+        //         topics.set(i, topic);
+        //         return;
+        //     }
+        // }
+    }
+
+    // modified in video 29
+    public void deleteTopic(String id) {
+        topicRepository.deleteById(id);
+        // topics.removeIf(t -> t.getId().equals(id));
+    }
+
+}
